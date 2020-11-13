9fe1a8d95daed1044a0ddd7cc25ad826b7f6f3f6
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-11-14 00:22:02 +0200

    Components part 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/Menu';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
             <NavbarBrand href='/'>Restaurent CON FUSION </NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     </>
   );
